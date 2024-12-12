e9aa869f3641f839776182eb285636efc54b5260
aniakarpliuk21 <aniakarpluk21@gmail.com>
2024-12-12 17:03:45 +0200

    master clear

--- Main.js
+++ Main.js
@@ -1,12 +1,1 @@
-const http = require('node:http');
-const serverFn = async () => {
-    const server = await http.createServer((req, res) => {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({
-            data: 'Hello World!',
-        }));
-    });
 
-    server.listen(8000);
-}
-serverFn();
