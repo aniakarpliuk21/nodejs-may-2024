b46b07a888143d763e2b1c30b83c860473ba7757
aniakarpliuk21 <aniakarpluk21@gmail.com>
2025-01-17 12:40:04 +0200

    HomeWork9 1

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -16,5 +16,11 @@ export const configure = {
   actionForgotPasswordSecret: process.env.ACTION_FORGOT_PASSWORD_SECRET,
   actionForgotPasswordExpiresIn: process.env.ACTION_FORGOT_PASSWORD_EXPIRES_IN,
   actionEmailVerificationSecret: process.env.ACTION_EMAIL_VERIFICATION_SECRET,
-  actionEmailVerificationExpiresIn: process.env.ACTION_EMAIL_VERIFICATION_EXPIRES_IN,
+  actionEmailVerificationExpiresIn:
+    process.env.ACTION_EMAIL_VERIFICATION_EXPIRES_IN,
+  AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+  AWS_SECRET_KEY: process.env.AWS_SECRET_KEY,
+  AWS_REGION: process.env.AWS_REGION,
+  AWS_S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME,
+  AWS_S3_ENDPOINT: process.env.AWS_S3_ENDPOINT,
 };
